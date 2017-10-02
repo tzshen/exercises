27575084dcef3bba948807aec7a505695cf7121a
Tianzuo Shen <tianzuo.shen@liteon.com>
2017-10-02 22:45:34 +0800

    Fix lint errors

--- webpack-demo/src/index.ts
+++ webpack-demo/src/index.ts
@@ -1,20 +1,20 @@
-import Vue from 'vue';
-import { cube } from './math.ts';
+import Vue from "vue";
+import { cube } from "./math";
 
-var app = new Vue({
-  el: '#app',
+const app = new Vue({
+  el: "#app",
   data: {
-    message: 'Hello Vue!'
-  }
+    message: "Hello Vue!",
+  },
 });
 
 function component() {
-  var element = document.createElement('pre');
-  var btn = document.createElement('button');
+  const element = document.createElement("pre");
+  const btn = document.createElement("button");
   element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
+    "Hello webpack!",
+    "5 cubed is equal to " + cube(5),
+  ].join("\n\n");
   return element;
 }
 
