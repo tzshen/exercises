81592dc9c4562fdfd61e06d8fd8b21ea7384a3fc
Tianzuo Shen <tianzuo.shen@liteon.com>
2017-10-04 15:29:27 +0800

    Add Vue.js demo

--- express-demo/myapp/public/javascripts/index.js
+++ express-demo/myapp/public/javascripts/index.js
@@ -1,5 +1,22 @@
 import { cube } from './math.js';
 
+Vue.component('simple-counter', {
+  template: '<button v-on:click="counter += 1">{{ counter }}</button>',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+});
+
+// eslint-disable-next-line no-unused-vars
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello, world.',
+  },
+});
+
 function component() {
   const element = document.createElement('pre');
   element.innerHTML = [
