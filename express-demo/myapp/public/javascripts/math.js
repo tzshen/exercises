b30ab881fc60627af018775e33c2d732f398f443
Tianzuo Shen <tianzuo.shen@liteon.com>
2017-10-04 23:19:39 +0800

    Add JSDoc comments

--- express-demo/myapp/public/javascripts/math.js
+++ express-demo/myapp/public/javascripts/math.js
@@ -1,7 +1,17 @@
+/**
+ * Compute square of a number
+ * @param {Number} x The number to compute square
+ * @returns {Number} The square of x
+ */
 export function square(x) {
   return x * x;
 }
 
+/**
+ * Compute cube of a number
+ * @param {Number} x The number to compute cube
+ * @returns {Number} The cube of x
+ */
 export function cube(x) {
   return x * x * x;
 }
