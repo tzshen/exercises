3fc97674762ba0200e0c2c2adb979fc7f998d6a9
Tianzuo Shen <tianzuo.shen@liteon.com>
2017-10-05 21:54:38 +0800

    Add demo of XMLHttpRequest and v-for

--- express-demo/myapp/public/javascripts/index.js
+++ express-demo/myapp/public/javascripts/index.js
@@ -14,6 +14,7 @@ const app = new Vue({
   el: '#app',
   data: {
     message: 'Hello, world.',
+    users: [],
   },
 });
 
@@ -27,3 +28,21 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+const request = new XMLHttpRequest();
+request.open('GET', '/users', true);
+
+request.onload = function onload() {
+  if (request.status >= 200 && request.status < 400) {
+    // Success!
+    app.users = JSON.parse(request.responseText);
+  } else {
+    // We reached our target server, but it returned an error')
+  }
+};
+
+request.onerror = function onerror() {
+  // There was a connection error of some sort
+};
+
+request.send();
--- express-demo/myapp/routes/users.js
+++ express-demo/myapp/routes/users.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
+const users = [
+  { name: 'Allen', age: 25 },
+  { name: 'Bill', age: 18 },
+  { name: 'Carol', age: 20 },
+  { name: 'Danny', age: 50 },
+];
+
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res/* , next */) => {
-  res.send('respond with a resource');
+  res.send(JSON.stringify(users));
 });
 
 module.exports = router;
